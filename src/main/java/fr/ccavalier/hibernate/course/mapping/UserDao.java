0044543aea8ce04be6c45a0dafe655a76a70f94a
fteychene <francois.teychene@tabmo.io>
2017-02-25 20:08:10 +0100

    Remove code for exercices

--- src/main/java/fr/ccavalier/hibernate/course/mapping/UserDao.java
+++ src/main/java/fr/ccavalier/hibernate/course/mapping/UserDao.java
@@ -1,5 +1,6 @@
 package fr.ccavalier.hibernate.course.mapping;
 //[imports] { autofold
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
@@ -13,6 +14,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 //}
+
 /**
  * Created by ccavalie on 31/01/2017.
  */
@@ -32,38 +34,34 @@ public class UserDao {
         Map<String, Object> params = new HashMap<String, Object>();
         params.put("name", name);
 
-        String sql = "SELECT * from USERS where first_name='"+name+"'";//Ecrire votre requete ici
+        String sql = "";//Ecrire votre requete ici pour recuperer le User
 
         User result = namedParameterJdbcTemplate.queryForObject(
                 sql,
                 params,
                 new UserMapper());
 
-        //new BeanPropertyRowMapper(Customer.class));
-        
-        String getMedias = "Select * from media_users as asso, media as m" +
-                "  where asso.id_user =:id_user and asso.id_media = m.id";
-        
-        params.clear();
-        params.put("id_user", result.getId());
+        String getMedias = "";//recuperer la liste de medias
 
+        params.clear();
+
+        //cette commande va executer la requete de requperation de medias
         List<Map<String, Object>> asso_media = namedParameterJdbcTemplate.queryForList(getMedias, params);
 
-        result.setContacts(UserMapper.mapContacts(asso_media));
         return result;
 
     }
 
 
-
     public void add(User user) {
         Map<String, Object> params = new HashMap<String, Object>();
-        String sql = "INSERT into users(first_name, last_name, address) values ('"+user.getFirstName()+"','"+user.getLastName()+"','"+user.getCity()+"')";
+        String sql = "";
         int value = namedParameterJdbcTemplate.update(sql, params);
     }
 
     /**
      * get all users from database
+     *
      * @return List<User> All Users
      */
     public List<User> findAll() {
@@ -73,27 +71,18 @@ public class UserDao {
         return result;
 
     }
-    
+
 
     private static final class UserMapper implements RowMapper<User> {
 
         public User mapRow(ResultSet rs, int rowNum) throws SQLException {
-            User user = new User();
-            user.setId(rs.getInt("id"));
-            user.setFirstName(rs.getString("first_name"));
-            user.setLastName(rs.getString("last_name"));
-            user.setCity(rs.getString("address"));
-            return user;
+            // Write the mapper for a User here
+            return null;
         }
 
         public static List<User.Media> mapContacts(List<Map<String, Object>> asso_media) {
             List<User.Media> mediaList = new ArrayList<User.Media>();
-            for(Map<String, Object> tuple : asso_media){
-                User.Media media = new User.Media();
-                media.setType((String)tuple.get("type"));
-                media.setValue((String)tuple.get("contact"));
-                mediaList.add(media);
-            }
+            // Mapper for type Media
             return mediaList;
         }
     }
--- src/main/java/fr/ccavalier/hibernate/course/requests/Requests.java
+++ src/main/java/fr/ccavalier/hibernate/course/requests/Requests.java
@@ -24,7 +24,7 @@ public class Requests {
      *
      */
     public List findNameQuantityInf10() {
-        String sql = "SELECT LIBELLE, ID FROM PRODUITS WHERE STOCK < 10";//Ecrire votre requete ici
+        String sql = "";//Ecrire votre requete ici
         List result = jdbcTemplate.queryForList(sql);
         return result;
 
@@ -35,7 +35,7 @@ public class Requests {
      * pour lesquels le délai d'approvisionnement est supérieur à 20 jours
      */
     public List findFourDelaiSup20(){
-        String sql = "SELECT f.nom, f.ville from fournisseurs f where f.id IN (SELECT a.id_four from acheter a WHERE a.delai > 20)";//Ecrire votre requete ici
+        String sql = "";//Ecrire votre requete ici
         List result = jdbcTemplate.queryForList(sql);
         return result;
     }
@@ -44,10 +44,8 @@ public class Requests {
      * Supprimer les fraises de la table des produits
      */
     public void deleteFraises(){
-        String sql = "DELETE FROM acheter WHERE ID_PROD=(SELECT id from produits where libelle = 'Fraises')";//Ecrire votre requete ici
+        String sql = "";//Ecrire votre requete ici
         jdbcTemplate.execute(sql);
-        String sql2 = "DELETE from produits where libelle = 'Fraises'";//Ecrire votre requete ici
-        jdbcTemplate.execute(sql2);
     }
 
     public List queryForList(String sql){
